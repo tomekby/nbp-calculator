8839cfeefd2cb1ad68dc498663a993020a14c454
Tomasz Stasiak <tomasz.stasiak@yahoo.pl>
2017-03-22 18:00:59 +0100

    added example of calculation

--- algorithm.php
+++ algorithm.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-$rangeStart = new DateTime('4 years ago');
+$rangeStart = new DateTime('10 years ago');
 $rangeEnd = new DateTime('now');
 
 $goldPricesFetcher = new \GoldSellGain\TimeRangeFetcher($rangeStart, $rangeEnd, new \GoldSellGain\Nbp\PriceFetcher);
@@ -11,4 +11,12 @@ $goldPrices = $goldPricesFetcher->fetch();
 $calculator = new \GoldSellGain\Calculator($goldPrices);
 $calculator->findBestGain();
 
-var_dump($calculator->getBestGain());
+$bestGainPerUnit = $calculator->getBestGain();
+$budget = 600000;
+
+$boughtUnits = $budget / $bestGainPerUnit['buyPrice'];
+$gain = $bestGainPerUnit['gain'] * $boughtUnits;
+
+echo "Najwiekszy zysk: {$gain} PLN\n";
+echo "Zakup: {$bestGainPerUnit['buyDate']}\n";
+echo "Sprzedaz: {$bestGainPerUnit['sellDate']}";
